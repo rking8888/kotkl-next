a30551f970a2752be01a1373fa6390efaf0477e6
Ryan King <rking8888@gmail.com>
2020-12-16 22:29:21 -0800

    Mongodb integration (#13)
    * Integrating mongoDB along with Apollo + GraphQL for querying
    
    * adding .js to eslint
    
    * allowing for graphql playground in production

---
+++ pages/api/graphql.js
@@ -0,0 +1,71 @@
+// pages/api/graphql.js
+import { ApolloServer, gql } from 'apollo-server-micro';
+import { makeExecutableSchema } from 'graphql-tools';
+import { MongoClient } from 'mongodb';
+
+require('dotenv').config();
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    blog: String
+    stars: Int
+  }
+
+  type Query {
+    users: [User]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users(_parent, _args, _context, _info) {
+      return _context.db
+        .collection('test')
+        .findOne()
+        .then((data) => {
+          return data.users;
+        });
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
+
+let db;
+
+const apolloServer = new ApolloServer({
+  schema,
+  context: async () => {
+    if (!db) {
+      try {
+        const dbClient = new MongoClient(process.env.MONGO_DB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        });
+
+        if (!dbClient.isConnected()) await dbClient.connect();
+        db = dbClient.db('kotkl'); // database name
+      } catch (e) {
+        console.log('--->error while connecting with graphql context (db)', e);
+      }
+    }
+
+    return { db };
+  }
+});
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
