2b2a7814149b98fbe7c573e2a28a06e56ce361ae
Ryan King <rking8888@gmail.com>
2020-12-29 17:24:06 -0800

    fix: adding expiry token to cookie and allowing for manual refresh, also updated ui look and feel

--- pages/api/auth/callback.js
+++ pages/api/auth/callback.js
@@ -11,7 +11,11 @@ export default (req, res) => {
     // console.log("PERSIST REFRESH TOKEN", refresh_token);
 
     res.setHeader('Set-Cookie', [
-      serialize('accessToken', access_token, { path: '/', httpOnly: true }),
+      serialize('accessToken', access_token, {
+        path: '/',
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 1),
+        httpOnly: true
+      }),
       serialize('refreshToken', refresh_token, {
         path: '/',
         httpOnly: true
